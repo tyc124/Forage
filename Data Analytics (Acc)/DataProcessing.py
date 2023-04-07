278ac68863be77fca09ea3dc932ce6842c4e94b5
tyc124 <77292020+tyc124@users.noreply.github.com>
2023-04-07 22:27:18 +0900

    Update and rename Accenture.py to DataProcessing.py

--- Data Analytics (Acc)/DataProcessing.py
+++ Data Analytics (Acc)/DataProcessing.py
@@ -10,14 +10,14 @@ reaction_db = reaction_db.dropna(how='any', axis=0)
 types_db = types_db.dropna(how='any', axis=0)
 
 for s in content_db['Category']:
-    s.replace('"', '') #some data is written with ""
+    s.replace('"', '') #some data is written with "" -> make it consistent
 
 
 #select the columns per needs only
 content_db.rename(columns = {'Type': 'Content Type'}, inplace=True)
 content_db = content_db[['Unnamed: 0', 'Content ID', 'Content Type', 'Category']]
 reaction_db.rename(columns = {'Type': 'Reaction Type'}, inplace=True)
-reaction_db = reaction_db[['Unnamed: 0', 'Content ID', 'Reaction Type', 'Datetime']] #keep datatime for traceability
+reaction_db = reaction_db[['Unnamed: 0', 'Content ID', 'Reaction Type', 'Datetime']] #keep datetime for traceability
 
 
 #merge the datasets
@@ -29,4 +29,5 @@ cleaned = pd.merge(cleaned, types_db, how='outer')
 print(cleaned['Category'].value_counts())
 #travel, science, culture, fitness, animals
 
+#save the result
 cleaned.to_csv(r'C:/Users/fanca/Desktop/CS/Forage/Accenture/result.csv', index=None, header=True)
